de5388d7eab5f49d418323fa6ac2f1323c991e5e
tragladit <tragladit@gmail.com>
2019-10-16 19:27:46 +0300

    Updates

---
+++ precache-manifest.5ea8e8263ffa3bd0b7479de5fa4d4c7a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "57ff0c57017a91128ecb",
+    "url": "/krossy/static/css/main.4d5c129a.chunk.css"
+  },
+  {
+    "revision": "57ff0c57017a91128ecb",
+    "url": "/krossy/static/js/main.57ff0c57.chunk.js"
+  },
+  {
+    "revision": "5ebd78a4d372e3aee23c",
+    "url": "/krossy/static/js/runtime~main.5ebd78a4.js"
+  },
+  {
+    "revision": "945744293b8f183a56f0",
+    "url": "/krossy/static/css/2.4a7b8c3f.chunk.css"
+  },
+  {
+    "revision": "945744293b8f183a56f0",
+    "url": "/krossy/static/js/2.94574429.chunk.js"
+  },
+  {
+    "revision": "81ec8c101b6bc92f7ae4e69db407f8eb",
+    "url": "/krossy/static/media/wb.81ec8c10.png"
+  },
+  {
+    "revision": "dc757a4e62f3140b58fd32a0430e06da",
+    "url": "/krossy/static/media/tag.dc757a4e.svg"
+  },
+  {
+    "revision": "7d2485413e1afa5c9d986e1388b89ac3",
+    "url": "/krossy/static/media/Rectangle 1533@2x.7d248541.png"
+  },
+  {
+    "revision": "8d3b5751a7810cfc9fead531063b49cf",
+    "url": "/krossy/static/media/backTinder.8d3b5751.svg"
+  },
+  {
+    "revision": "dcf70311333833bd34f350118ef3f644",
+    "url": "/krossy/static/media/SFUIText-Light.dcf70311.woff"
+  },
+  {
+    "revision": "1e1d803a7a614c854ce213acf143edc2",
+    "url": "/krossy/static/media/SFUIText-Regular.1e1d803a.ttf"
+  },
+  {
+    "revision": "e4286ca7dd7b3af5fad228ebcb84842b",
+    "url": "/krossy/static/media/SFUIText-Medium.e4286ca7.ttf"
+  },
+  {
+    "revision": "34dfe822161ceb31dc5889d742ab591c",
+    "url": "/krossy/static/media/SFUIText-Semibold.34dfe822.ttf"
+  },
+  {
+    "revision": "49d167fd00ec879e285af5661cc763aa",
+    "url": "/krossy/static/media/SFUIText-Bold.49d167fd.ttf"
+  },
+  {
+    "revision": "0e943850ffe77597e9570f008d7cae78",
+    "url": "/krossy/static/media/iOS 1.0e943850.svg"
+  },
+  {
+    "revision": "d7d002a426ec0328e363b2095e1661ff",
+    "url": "/krossy/static/media/iOS 2.d7d002a4.svg"
+  },
+  {
+    "revision": "c988a16c062dfa96c091ccbdc3b63713",
+    "url": "/krossy/static/media/iOS 3.c988a16c.svg"
+  },
+  {
+    "revision": "aaf41a4e9e738fda9f3b494e5516bcad",
+    "url": "/krossy/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/krossy/precache-manifest.b8b23be645d179d6ace8fe5d3ed15291.js"
+  "/krossy/precache-manifest.5ea8e8263ffa3bd0b7479de5fa4d4c7a.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/krossy/index.html"), {
+workbox.routing.registerNavigationRoute("/krossy/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
