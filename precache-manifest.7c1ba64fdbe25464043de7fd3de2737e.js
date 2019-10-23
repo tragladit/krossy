2c0740bdc9061dc668d993bdaa1e801f7c9cbcc3
tragladit <tragladit@gmail.com>
2019-10-23 10:02:52 +0300

    Updates

---
+++ precache-manifest.7c1ba64fdbe25464043de7fd3de2737e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57f815fb4fd6ebf531055709ebe5ec22",
+    "url": "/krossy/index.html"
+  },
+  {
+    "revision": "00c0d5ce63ebf6711044",
+    "url": "/krossy/static/css/2.7214840c.chunk.css"
+  },
+  {
+    "revision": "5d1a50ebef1d3e2df751",
+    "url": "/krossy/static/css/main.5a85b792.chunk.css"
+  },
+  {
+    "revision": "00c0d5ce63ebf6711044",
+    "url": "/krossy/static/js/2.7ed18e3a.chunk.js"
+  },
+  {
+    "revision": "5d1a50ebef1d3e2df751",
+    "url": "/krossy/static/js/main.70e7750e.chunk.js"
+  },
+  {
+    "revision": "3d159f25f5167fd4be43",
+    "url": "/krossy/static/js/runtime-main.3baf317b.js"
+  },
+  {
+    "revision": "7d2485413e1afa5c9d986e1388b89ac3",
+    "url": "/krossy/static/media/Rectangle 1533@2x.7d248541.png"
+  },
+  {
+    "revision": "49d167fd00ec879e285af5661cc763aa",
+    "url": "/krossy/static/media/SFUIText-Bold.49d167fd.ttf"
+  },
+  {
+    "revision": "dcf70311333833bd34f350118ef3f644",
+    "url": "/krossy/static/media/SFUIText-Light.dcf70311.woff"
+  },
+  {
+    "revision": "e4286ca7dd7b3af5fad228ebcb84842b",
+    "url": "/krossy/static/media/SFUIText-Medium.e4286ca7.ttf"
+  },
+  {
+    "revision": "1e1d803a7a614c854ce213acf143edc2",
+    "url": "/krossy/static/media/SFUIText-Regular.1e1d803a.ttf"
+  },
+  {
+    "revision": "34dfe822161ceb31dc5889d742ab591c",
+    "url": "/krossy/static/media/SFUIText-Semibold.34dfe822.ttf"
+  },
+  {
+    "revision": "8d3b5751a7810cfc9fead531063b49cf",
+    "url": "/krossy/static/media/backTinder.8d3b5751.svg"
+  },
+  {
+    "revision": "0e943850ffe77597e9570f008d7cae78",
+    "url": "/krossy/static/media/iOS 1.0e943850.svg"
+  },
+  {
+    "revision": "d7d002a426ec0328e363b2095e1661ff",
+    "url": "/krossy/static/media/iOS 2.d7d002a4.svg"
+  },
+  {
+    "revision": "c988a16c062dfa96c091ccbdc3b63713",
+    "url": "/krossy/static/media/iOS 3.c988a16c.svg"
+  },
+  {
+    "revision": "dc757a4e62f3140b58fd32a0430e06da",
+    "url": "/krossy/static/media/tag.dc757a4e.svg"
+  },
+  {
+    "revision": "81ec8c101b6bc92f7ae4e69db407f8eb",
+    "url": "/krossy/static/media/wb.81ec8c10.png"
+  }
+]);
